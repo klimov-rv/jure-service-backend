ed8e5ecfeb524873610c1b7735bd88b47de11496
Klimov Roman <klimov.rv@ya.ru>
2023-05-30 19:16:10 +0300

    some models

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\DocTemplateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+// Route::middleware('auth')->group(function () { 
+// });
+
+Route::apiResources([
+    'docs' => DocumentController::class,
+    'doctemplates' => DocTemplateController::class,
+]); 
