9ff002f47456691593100a03c856de7b45240fac
Klimov Roman <klimov.rv@ya.ru>
2023-05-31 15:51:48 +0300

    set-url

--- database/migrations/2023_05_30_145344_create_documents_table.php
+++ database/migrations/2023_05_30_145344_create_documents_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
-            // проверить связь "один ко многим"
+            // TODO проверить связь "один ко многим"
             $table->foreignId('doc_template_id');
             $table->string('name');
             $table->string('category');
