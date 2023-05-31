269d22e658d6305591294d62f5dc5bd1a2b31d28
Klimov Roman <klimov.rv@ya.ru>
2023-05-31 15:53:10 +0300

    TODO relations

--- database/migrations/2023_05_30_145344_create_documents_table.php
+++ database/migrations/2023_05_30_145344_create_documents_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
-            // TODO проверить связь "один ко многим"
+            // TODO relations docTemplates hasMany Documents
             $table->foreignId('doc_template_id');
             $table->string('name');
             $table->string('category');
